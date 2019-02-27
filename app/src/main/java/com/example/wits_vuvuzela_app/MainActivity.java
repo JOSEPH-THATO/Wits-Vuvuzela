ff317eb862721c802300d62905475d3be0c21467
Ayo <1386422@students.wits.ac.za>
2019-02-27 12:59:03 +0200

    Backend of the User registration and login page

--- app/src/main/java/com/example/wits_vuvuzela_app/MainActivity.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.wits_vuvuzela_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText Username;
+    EditText Password;
+    Button Login;
+    TextView ForgotPassword;
+    TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = (EditText)findViewById(R.id.EdtxtUsernameLogin);
+        Password = (EditText)findViewById(R.id.EdTxtPasswordLogin);
+        Login = (Button)findViewById(R.id.BtnLogin);
+        ForgotPassword = (TextView)findViewById(R.id.txtViewForgotPasswordLogin);
+        Register = (TextView)findViewById(R.id.txtViewRegisterLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnterHomePage();
+            }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnterRegistrationPage();
+            }
+        });
+
+    }
+
+    private void EnterRegistrationPage() {
+
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
+
+
+    }
+
+    private void EnterHomePage() {
+
+        //Intent intent = new Intent(this,Register.class);
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/wits_vuvuzela_app/Register.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/Register.java
@@ -1,13 +1,58 @@
 package com.example.wits_vuvuzela_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+
+    EditText FirstName;
+    EditText LastName;
+    EditText Username;
+    EditText EmailAddress;
+    EditText Password;
+    EditText PasswordConfirmation;
+    Button Register;
+    TextView Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        FirstName = (EditText)findViewById(R.id.EdtxtFirstNameReg);
+        LastName = (EditText)findViewById(R.id.EdtxtLastNameReg);
+        Username = (EditText)findViewById(R.id.EdtxtUsernameReg);
+        EmailAddress = (EditText)findViewById(R.id.EdtxtEmailAddressReg);
+        Password = (EditText)findViewById(R.id.EdtxtPasswordReg);
+        PasswordConfirmation = (EditText)findViewById(R.id.EdtxtPasswordConfirmationReg);
+        Register = (Button)findViewById(R.id.BtnRegisterReg);
+        Login = (TextView)findViewById(R.id.txtViewLoginReg);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnterLoginPage();
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnterLoginPage();
+            }
+        });
     }
+
+    private void EnterLoginPage() {
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
