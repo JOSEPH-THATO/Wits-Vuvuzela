063694a33fe63a1413a444972c9c2356a6c753b1
EdwinMD3 <1406391@students.wits.ac.za>
2019-03-04 15:12:23 +0200

    TG-16 #In-Progress

--- app/src/main/java/com/example/wits_vuvuzela_app/Register.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/Register.java
@@ -1,12 +1,19 @@
 package com.example.wits_vuvuzela_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
@@ -19,10 +26,14 @@ public class Register extends AppCompatActivity {
     Button Register;
     TextView Login;
 
+    private FirebaseAuth firebaseauth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseauth = FirebaseAuth.getInstance();
 
         FirstName = (EditText)findViewById(R.id.EdtxtFirstNameReg);
         LastName = (EditText)findViewById(R.id.EdtxtLastNameReg);
@@ -36,7 +47,10 @@ public class Register extends AppCompatActivity {
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EnterLoginPage();
+               // EnterLoginPage();
+
+
+                registerUser();
             }
         });
 
@@ -47,6 +61,33 @@ public class Register extends AppCompatActivity {
             }
         });
     }
+
+    private void registerUser(){
+
+        String user_fName = FirstName.getText().toString().trim();
+        String User_lName = LastName.getText().toString().trim();
+        String User_username = Username.getText().toString().trim();
+        String User_email = EmailAddress.getText().toString().trim();
+        String User_password = Password.getText().toString().trim();
+
+
+        firebaseauth.createUserWithEmailAndPassword(User_email,User_password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(Register.this, "successful", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(Register.this, "failed", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+    }
+
 
     private void EnterLoginPage() {
 
