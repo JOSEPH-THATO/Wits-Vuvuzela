c4e1602e133c59a94ff3de20ec2092eb502cb9be
JOSEPH-KEKANA <1503246@students.wits.ac.za>
2019-03-06 10:44:23 +0200

    User can now login TG-23 #Ready-for-test

--- app/src/main/java/com/example/wits_vuvuzela_app/MainActivity.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.wits_vuvuzela_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,31 +23,35 @@ public class MainActivity extends AppCompatActivity {
     Button Login;
     TextView ForgotPassword;
     TextView Register;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-<<<<<<< HEAD
         Username = (EditText) findViewById(R.id.EdtxtUsernameLogin);
         Password = (EditText) findViewById(R.id.EdTxtPasswordLogin);
         Login = (Button) findViewById(R.id.BtnLogin);
         ForgotPassword = (TextView) findViewById(R.id.txtViewForgotPasswordLogin);
         Register = (TextView) findViewById(R.id.txtViewRegisterLogin);
-=======
 
-        Username = (EditText)findViewById(R.id.EdtxtUsernameLogin);
-        Password = (EditText)findViewById(R.id.EdTxtPasswordLogin);
-        Login = (Button)findViewById(R.id.BtnLogin);
-        ForgotPassword = (TextView)findViewById(R.id.txtViewForgotPasswordLogin);
-        Register = (TextView)findViewById(R.id.txtViewRegisterLogin);
->>>>>>> add37b5e00032d892a2929b810733268a1ce2650
-
+        firebaseAuth = FirebaseAuth.getInstance();
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EnterHomePage();
+                firebaseAuth.signInWithEmailAndPassword(Username.getText().toString(),Password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                      if(task.isSuccessful()){
+
+                          EnterHomePage();
+                      }
+                      else {
+
+                          Toast.makeText(MainActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                      }
+                    }
+                });
             }
         });
 
@@ -64,8 +73,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void EnterHomePage() {
 
-        //Intent intent = new Intent(this,Register.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
     }
 
 
