e6063a521eb1ddb206e47ae9535ba8de398f2051
Ayo <1386422@students.wits.ac.za>
2019-04-02 13:22:26 +0200

    TG-37 #In-progress

--- app/src/main/java/com/example/wits_vuvuzela_app/HomePage.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/HomePage.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +33,38 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
      /**/
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+          return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.articlelayout,null);
+
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.ArticleImage);
+            TextView textView_heading =(TextView)convertView.findViewById(R.id.ArticleHeading);
+            TextView textView_author = (TextView)convertView.findViewById(R.id.ArticleAuthor);
+
+            //imageView.setImageResource(IMAGES[position]);
+            //textView_heading.setText(NAMES[position]);
+            //textView_author.setText(AUTHORNAMES[position]);
+            return null;
+        }
     }
 
 }
