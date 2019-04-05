5dc73a50bb302f997dd083934a1a9e5da8bf6de1
JOSEPH-KEKANA <1503246@students.wits.ac.za>
2019-04-05 14:55:29 +0200

    Fixed Error in Class

--- app/src/main/java/com/example/wits_vuvuzela_app/Article.java
+++ app/src/main/java/com/example/wits_vuvuzela_app/Article.java
@@ -7,44 +7,4 @@ import java.util.Date;
 
 public class Article {
 
-    String ArticleImage;
-    String ArticleTitle;
-    String ArticleAutherName;
-
-    public Image getArticleImage() {
-        return ArticleImage;
-    }
-
-    public void setArticleImage(Image articleImage) {
-        ArticleImage = articleImage;
-    }
-
-    public String getArticleTitle() {
-        return ArticleTitle;
-    }
-
-    public void setArticleTitle(String articleTitle) {
-        ArticleTitle = articleTitle;
-    }
-
-    public String getArticleAutherName() {
-        return ArticleAutherName;
-    }
-
-    public void setArticleAutherName(String articleAutherName) {
-        ArticleAutherName = articleAutherName;
-    }
-
-    public Date getArticleDateUploaded() {
-        return ArticleDateUploaded;
-    }
-
-    public void setArticleDateUploaded(Date articleDateUploaded) {
-        ArticleDateUploaded = articleDateUploaded;
-    }
-
-
-    public void AddComment(String addComment){
-        ArticleComments.add(addComment);
-    }
 }
